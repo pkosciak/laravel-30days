54845f2d91a6a3d841bad1afc518b612101ddbd9
pkosciak <pkosciak@webpacket.io>
2024-06-27 23:25:06 +0200

    Add layout component

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 
 Route::get('/about', function () {
-    return view('welcome');
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
